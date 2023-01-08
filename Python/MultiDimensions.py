be5d4c244ceea21748d91f02f403c4d0c49fd1f1
Chloe Zeng <98899028+DummyPuppy@users.noreply.github.com>
2023-01-08 16:32:07 -0500

    Update MultiDimensions.py
    insert interval question

--- Python/MultiDimensions.py
+++ Python/MultiDimensions.py
@@ -9,3 +9,21 @@ class Solution:
             if i+nums[i] >=goal:
                 goal = i
         return True if goal == 0 else False
+
+    
+  #insert intervals
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = list()
+        for i in range(len(intervals)):
+            temp = intervals[i]
+            if newInterval[1] < temp[0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0] > temp[1]:
+                result.append(temp)
+            else: 
+                newInterval = [min(newInterval[0],temp[0]),max(newInterval[1],temp[1])]
+        result.append(newInterval)
+        return result
+            
