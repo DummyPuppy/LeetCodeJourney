cb5da36e0ff5a4ce4cffbfced161a7c8cf626b18
Chloe Zeng <98899028+DummyPuppy@users.noreply.github.com>
2023-02-09 11:08:34 -0500

    add a stack related problem - letter combo of d

--- Python/MediumCase.py
+++ Python/MediumCase.py
@@ -21,4 +21,30 @@ class Solution:
 
 
       
-      
+      #letter combo of digits
+    #use stack to store the previous combos till the full ones
+    class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) ==0:
+            return []
+        
+        map = dict()
+        map['2'] = ['a','b','c']
+        map['3'] = ['d','e','f']
+        map['4'] = ['g','h','i']
+        map['5'] = ['j','k','l']
+        map['6'] = ['m','n','o']
+        map['7'] = ['p','q','r','s']
+        map['8'] = ['t','u','v']
+        map['9'] = ['w','x','y','z']
+        stack = [(0,'')]
+        combo = []
+        while stack:
+            idx, char = stack.pop()
+            if idx == len(digits):
+                combo.append(char)
+            else:
+                for letter in map[digits[idx]]:
+                    stack.append((idx+1, char + letter))
+        return combo
+                
