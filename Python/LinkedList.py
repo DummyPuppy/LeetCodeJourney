68d83527ac12ca8a5226adec6ec93bdef92b45d0
Chloe Zeng <98899028+DummyPuppy@users.noreply.github.com>
2023-02-14 22:46:48 -0500

    Update LinkedList.py

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -24,3 +24,35 @@ class Solution:
         curr.next = None
         temp.next = head
         return newHead 
+
+    # to sort two sorted linkedlists
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not (list1 or list2):
+            return None
+        curr = dummy = ListNode()
+
+        p1,p2 = list1, list2
+        while (p1 and p2):
+            if (p1.val < p2.val):
+                curr.next = p1
+                curr = curr.next
+                p1 = p1.next
+            else:
+                curr.next = p2
+                curr = curr.next
+                p2 = p2.next
+        while p1:
+            curr.next = p1
+            curr = curr.next
+            p1 = p1.next
+        while p2:
+            curr.next = p2
+            curr = curr.next
+            p2 = p2.next
+        return dummy.next
